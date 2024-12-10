aba95ebfd30436909534f8da8074e0df1fd2a11c
Karthikeyan <karthikeyan.kannan37@gmail.com>
2024-12-10 07:36:51 +0000

    Supports .docx & chunk size and overlap changed

--- sayvai_rag/text_splitter.py
+++ sayvai_rag/text_splitter.py
@@ -1,8 +1,8 @@
-from langchain_community.document_loaders import CSVLoader, TextLoader, PyPDFLoader, JSONLoader, UnstructuredHTMLLoader, UnstructuredMarkdownLoader
+from langchain_community.document_loaders import CSVLoader, TextLoader, PyPDFLoader, JSONLoader, UnstructuredHTMLLoader, UnstructuredMarkdownLoader, Docx2txtLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 # Function to load and split the PDF
-def load_and_split_files(file_path, chunk_size=1024, chunk_overlap=10):
+def load_and_split_files(file_path, chunk_size=500, chunk_overlap=20):
 
     file_extension = os.path.splitext(file_path)[1]
 
@@ -14,6 +14,9 @@ def load_and_split_files(file_path, chunk_size=1024, chunk_overlap=10):
 
     elif file_extension == ".json":
         loader = JSONLoader(file_path)
+
+    elif file_extension == ".docx":
+        loader = Docx2txtLoader(file_path)
 
     elif file_extension == ".html":
         loader = UnstructuredHTMLLoader(file_path)
